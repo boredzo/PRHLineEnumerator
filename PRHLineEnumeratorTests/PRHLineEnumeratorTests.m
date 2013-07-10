5daeac161b88a2cc5c07a8b433372f2f3a27371b
Peter Hosey <git@boredzo.org>
2013-07-10 15:58:29 -0700

    Code style in empty prefab test class. (Super important.)

--- PRHLineEnumeratorTests/PRHLineEnumeratorTests.m
+++ PRHLineEnumeratorTests/PRHLineEnumeratorTests.m
@@ -10,23 +10,20 @@
 
 @implementation PRHLineEnumeratorTests
 
-- (void)setUp
-{
-    [super setUp];
-    
-    // Set-up code here.
+- (void) setUp {
+	[super setUp];
+
+	// Set-up code here.
 }
 
-- (void)tearDown
-{
-    // Tear-down code here.
-    
-    [super tearDown];
+- (void) tearDown {
+	// Tear-down code here.
+
+	[super tearDown];
 }
 
-- (void)testExample
-{
-    STFail(@"Unit tests are not implemented yet in PRHLineEnumeratorTests");
+- (void) testExample {
+	STFail(@"Unit tests are not implemented yet in PRHLineEnumeratorTests");
 }
 
 @end
