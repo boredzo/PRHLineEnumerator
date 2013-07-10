b170a67d2264e8cb252edcda259046f8a1c8d0d0
Peter Hosey <git@boredzo.org>
2013-07-10 15:12:48 -0700

    Added an Xcode project with a unit-test bundle target.
    Includes Warnings.xcconfig in a submodule.

---
+++ PRHLineEnumeratorTests/PRHLineEnumeratorTests.h
@@ -0,0 +1,13 @@
+//
+//  PRHLineEnumeratorTests.h
+//  PRHLineEnumeratorTests
+//
+//  Created by Peter Hosey on 2013-07-10.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface PRHLineEnumeratorTests : SenTestCase
+
+@end
---
+++ PRHLineEnumeratorTests/PRHLineEnumeratorTests.m
@@ -0,0 +1,32 @@
+//
+//  PRHLineEnumeratorTests.m
+//  PRHLineEnumeratorTests
+//
+//  Created by Peter Hosey on 2013-07-10.
+//
+//
+
+#import "PRHLineEnumeratorTests.h"
+
+@implementation PRHLineEnumeratorTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in PRHLineEnumeratorTests");
+}
+
+@end
